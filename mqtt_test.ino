3efc8d0f130764ac8ddcf349f071fa977289bf10
vokhlebalov <vokhlebalov@edu.hse.ru>
2022-03-14 01:25:50 +0500

    Added mqtt.test file

---
+++ mqtt_test.ino
@@ -0,0 +1,155 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+
+/*
+  TODO
+  1. Добавить переподключение клиента при ошибке подключения
+  2. Выделить генерацию данных в отдельную функцию
+  3. Добавить дополнительные переменные для отправки
+  4. 
+  
+*/
+
+// Function prototypes
+//void subscribeReceive(char* topic, byte* payload, unsigned int length);
+void callback(char* topic, byte* payload, unsigned int length);
+void reconnect();
+
+// Set your MAC address and IP address here
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192, 168, 1, 160);
+ 
+// Make sure to leave out the http and slashes!
+const char* server = "82.148.31.128";
+
+//mqtt.eclipseprojects.io
+//www.mqtt-dashboard.com
+//test.mosquitto.org
+// 82.148.31.128
+
+// Ethernet and MQTT related objects
+EthernetClient ethClient;
+PubSubClient mqttClient(ethClient);
+
+const char clientID[] = "someClient123";
+const char outTopic[] = "mega_1/temp";
+const char inTopic[] = "mega_1/temp";
+long lastReconnectAttempt = 0;
+
+void setup() {
+  
+  // Useful for debugging purposes
+  Serial.begin(9600);
+  
+  // start the Ethernet connection:
+  Serial.println("Initialize Ethernet with DHCP:");
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    
+    // Check for Ethernet hardware present
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      while (true) {
+        delay(1); // do nothing, no point running without Ethernet hardware
+      }
+    }
+    if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println("Ethernet cable is not connected.");
+    }
+    
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip);
+  } else {
+    Serial.print("  DHCP assigned IP "); Serial.println(Ethernet.localIP());
+  }
+
+  // give the Ethernet shield a second to initialize:
+  delay(3000);
+  Serial.print("connecting to "); Serial.print(server); Serial.println("...");
+  
+  // Set the MQTT server to the server stated above ^
+  mqttClient.setServer(server, 1883);   
+  // Establish the subscribe event
+  mqttClient.setCallback(callback);
+  
+  // Attempt to connect to the server with the clientID
+  if (mqttClient.connect(clientID)) 
+  {
+    Serial.println("Connection has been established, well done");
+  } 
+  else 
+  {
+    Serial.println("Looks like the server connection failed...");
+  }
+
+  lastReconnectAttempt = 0;
+}
+
+void loop() {
+
+//  if (!client.connected()) {
+//    
+//  }
+  
+  // This is needed at the top of the loop!
+  mqttClient.loop();
+ 
+  // Ensure that we are subscribed to the topic "MakerIOTopic"
+  mqttClient.subscribe("mega_1/temp");
+
+  String buf = String(random(1, 250));
+  int bufLength = buf.length() + 1;
+  char data[bufLength];
+  buf.toCharArray(data, bufLength);
+ 
+  // Attempt to publish a value to the topic "MakerIOTopic"
+  if(mqttClient.publish("mega_1/temp", data))
+  {
+    Serial.println("Publish message success");
+  }
+  else
+  {
+    Serial.println("Could not send message :(");
+  }
+ 
+  // Dont overload the server!
+  delay(1000);
+}
+
+void reconnect() {
+  if (mqttClient.connect("arduinoClient")) {
+    // Once connected, publish an announcement...
+    mqttClient.publish(outTopic,"hello world");
+    // ... and resubscribe
+    mqttClient.subscribe(inTopic);
+  }
+//  return mqttClient.connected();
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+//void subscribeReceive(char* topic, byte* payload, unsigned int length)
+//{
+//  // Print the topic
+//  Serial.print("Topic: ");
+//  Serial.println(topic);
+// 
+//  // Print the message
+//  Serial.print("Message: ");
+//  for(int i = 0; i < length; i ++)
+//  {
+//    Serial.print(char(payload[i]));
+//  }
+// 
+//  // Print a newline
+//  Serial.println("");
+//}
